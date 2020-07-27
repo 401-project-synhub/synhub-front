2520e79b27434357b644d8668c4f23be79e67ce1
Tommalieh <muhammedtommalieh@gmail.com>
2020-07-27 16:50:52 +0300

    homepage

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './header/header.js'
+import Home from './home/home.js'
+
+import './app.scss';
+function App() {
+  return (
+    <>
+    <Header />
+    <main>
+      <Home />
+    </main>
+    </>
+  )
+}
+
+export default App;
---
+++ src/header/header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './header.scss';
+
+function Header(props) {
+
+    return (
+        <section className='header'>
+            {/* <div> */}
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Community</li>
+                        <li>Code Along</li>
+                    </ul>
+                </nav>
+            {/* </div> */}
+            <div>
+                <h2>Learn and Teach</h2>
+                <h1>At SynHub </h1>
+                <p>Adipisicing elit, sed do eiusmod tempor incididunt with labore et dolore magna aliqua enim ad minimum.</p>
+            </div>
+            <img id='hdr-illus' src='assets/header-illus2.png' alt='asset'></img>
+            <img id='hdr-dimond1' className='hdr-dimond' src='assets/dimond-shape.png' alt='asset'></img>
+            <img id='hdr-dimond2' className='hdr-dimond' src='assets/dimond-shape.png' alt='asset'></img>
+            <img id='hdr-dimond3' className='hdr-dimond' src='assets/dimond-shape.png' alt='asset'></img>
+
+        </section>
+    )
+}
+
+export default Header;
