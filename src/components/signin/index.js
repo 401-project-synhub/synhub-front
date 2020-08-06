ee4e959b856bd6dafd1f0d9691a735c9ce094cc6
Tommalieh <muhammedtommalieh@gmail.com>
2020-08-06 06:23:10 +0300

    css3

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -30,30 +30,23 @@ class SignIn extends React.Component {
     console.log('hello sign in')
     return (
       <>
-        {/* <Show condition={this.context.signedIn}>
-          <Button className="btn btn-primary btn-large centerButton" size="sm" type="submit" onClick={this.context.signout}>Logout</Button>
-        </Show> */}
+   
         <div className="bgBlack">
           <div className="sign-popup">
             <Show condition={!this.context.signedIn}>
+          <span id='close' onClick={this.context.changeOpen}>X</span>
               <h2>Sign In</h2>
               <Form role="form" onSubmit={this.handleSubmit} >
-                {/* <Form.Label> */}
-                  {/* <span>User Name</span> */}
                   <Form.Control
                     placeholder="username"
                     name="username"
                     onChange={this.handleChange}
                   />
-                {/* </Form.Label> */}
-                {/* <Form.Label> */}
-                  {/* <span>Password</span> */}
                   <Form.Control
                     placeholder="password"
                     name="password"
                     onChange={this.handleChange}
                   />
-                {/* </Form.Label> */}
                 <Button size="sm" className="btn btn-primary btn-large centerButton" type="submit">Login</Button>
               </Form>
             </Show>
